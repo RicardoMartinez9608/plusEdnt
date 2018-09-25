f11020ebcbd136cae9eb4cd56d4428834f6ca85a
ricar <ricar@DESKTOP-O1UUVPI>
2018-09-25 15:43:57 -0600

    clase conexion

--- Edent+/src/conexionSQL/InsertarDatosBD.java
+++ Edent+/src/conexionSQL/InsertarDatosBD.java
@@ -19,15 +19,15 @@ public class InsertarDatosBD {
      public void insertarPaciente(String nombre, String apellido ,String direccion, String telefono,String edad,String tipo,String fecha){
         try {
             PreparedStatement pps = cn.prepareStatement("INSERT INTO paciente(`nombre`,`apellido`,"
-                    + "`direccion`,`telefono`,`edad`,`fecha_registro`,`(id_tipop`)"
+                    + "`direccion`,`telefono`,`edad`,`fecha_registro`,`id_tipop`)"
                     + "VALUES(?,?,?,?,?,?,?);");
             pps.setString(1, nombre);
             pps.setString(2, apellido);
             pps.setString(3, direccion);
             pps.setString(4, telefono);
-            pps.setString(5, tipo);
-            pps.setString(6, edad);
-            pps.setString(7, fecha);
+            pps.setString(5, edad);
+            pps.setString(6, fecha);
+            pps.setString(7, tipo);
             pps.executeUpdate();
             JOptionPane.showMessageDialog(null, "Datos Guardados");
             con.closeBD();
