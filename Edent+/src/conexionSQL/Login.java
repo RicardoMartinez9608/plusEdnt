cf2be89b48431d107e11104a8c0575a0cb28c7b1
mauba <mauba@MB>
2018-10-22 22:46:31 -0600

    Mauricio: metodos para registrarse por primera vez, y metodos para login

--- Edent+/src/conexionSQL/Login.java
+++ Edent+/src/conexionSQL/Login.java
@@ -1,6 +1,50 @@
 
 package conexionSQL;
+import java.sql.CallableStatement;
+import javax.swing.JOptionPane;
 
 public class Login {
     
+        ConexionDB bd= new ConexionDB();
+    //funcion para registrar el usuario por primera vez
+    public String procedureRegistarLogin(String Nombre, String Apellido, String Usuario,
+            String Contrasenia, String Correo, String TipoUsser)
+    {
+        String resultado=null;
+        try {
+                CallableStatement proc = bd.conn.prepareCall("{CALL InUsuario(?,?,?,?,?,?,?)}");
+                proc.setString("pNombre",Nombre );
+                proc.setString("pApellido",Apellido );
+                proc.setString("pUsuario",Usuario );
+                proc.setString("pContra",Contrasenia );
+                proc.setString("pCorreo",Correo );
+                proc.setString("pTipoU",TipoUsser );
+                proc.registerOutParameter("pMjs", java.sql.Types.VARCHAR);
+                proc.executeQuery();           
+
+                resultado = proc.getString("pMjs");
+            } 
+           catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e);
+           }
+           return resultado;
+        }
+        //funcion para iniciar secion
+        public Integer procedureLogin(String Contrasenia, String Correo)
+        {
+            Integer resultado=null;
+           try {
+                CallableStatement proc = bd.conn.prepareCall("{CALL LogIn(?,?,?)}");
+                proc.setString("pContra",Contrasenia );
+                proc.setString("pCorreo",Correo );
+                proc.registerOutParameter("pmsj", java.sql.Types.INTEGER);
+                proc.executeQuery();            
+
+                resultado = proc.getInt("pmsj");
+            } 
+           catch (Exception e) {                  
+            // JOptionPane.showMessageDialog(null, e);
+           }
+           return resultado;
+        }
 }
