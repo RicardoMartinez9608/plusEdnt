b35677b01fe22ac0ad911d2db8eaaf7c05c50a9d
DELL <DELL@192.168.56.1>
2018-11-15 15:47:47 -0600

    trataor jpa

--- Edent+/src/formularios/frmPrincipal.java
+++ Edent+/src/formularios/frmPrincipal.java
@@ -251,7 +251,7 @@ public class frmPrincipal extends javax.swing.JFrame implements  Runnable {
                     .addComponent(jLabel1)
                     .addComponent(jLabel2)
                     .addComponent(lbhora))
-                .addGap(68, 68, 68))
+                .addGap(35, 35, 35))
         );
 
         jMenu3.setForeground(new java.awt.Color(0, 102, 153));
