590619d39dba23c6b6a51abbd5872069cfb6e617
mauba <mauba@MB>
2018-11-14 23:23:32 -0600

    Mauricio: Ya funciona el login e Instalacion

--- Edent+/src/Clases/cSesion.java
+++ Edent+/src/Clases/cSesion.java
@@ -3,6 +3,7 @@ package Clases;
 
 import java.sql.CallableStatement;
 import javax.swing.JOptionPane;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class cSesion extends ConexionDB {
 //    ConexionDB bd= new ConexionDB();
@@ -12,17 +13,17 @@ public class cSesion extends ConexionDB {
     {
         String resultado=null;
         try {
-                CallableStatement proc = conn.prepareCall("{CALL InUsuario(?,?,?,?,?,?,?)}");
+                CallableStatement proc = ConexionDB().prepareCall("{CALL InUsuario(?,?,?,?,?,?,?)}");
                 proc.setString("pNombre",Nombre );
                 proc.setString("pApellido",Apellido );
                 proc.setString("pUsuario",Usuario );
-                proc.setString("pContra",Contrasenia );
+                proc.setString("pContra",Contrasenia);
                 proc.setString("pCorreo",Correo );
                 proc.setString("pTipoU",TipoUsser );
-                proc.registerOutParameter("pMjs", java.sql.Types.VARCHAR);
+                proc.registerOutParameter("pmsj", java.sql.Types.VARCHAR);
                 proc.executeQuery();           
 
-                resultado = proc.getString("pMjs");
+                resultado = proc.getString("pmsj");
             } 
            catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e);
@@ -30,21 +31,21 @@ public class cSesion extends ConexionDB {
            return resultado;
         }
         //funcion para iniciar secion
-        public Integer procedureLogin(String Contrasenia, String Correo)
+        public Integer procedureLogin(String usuario, String Contrasenia)
         {
             Integer resultado=0;
-           try {
-                CallableStatement proc = conn.prepareCall("{CALL LogIn(?,?,?)}");
-                proc.setString("pContra",Contrasenia );
-                proc.setString("pCorreo",Correo );
+            try {
+                CallableStatement proc = ConexionDB().prepareCall("{CALL LogIn(?,?,?)}");                
+                proc.setString("pUsser",usuario );
+                proc.setString("pContra",Contrasenia);
                 proc.registerOutParameter("pmsj", java.sql.Types.INTEGER);
                 proc.executeQuery();            
 
                 resultado = proc.getInt("pmsj");
             } 
-           catch (Exception e) {                  
-            // JOptionPane.showMessageDialog(null, e);
-           }
-           return resultado;
+            catch (Exception e) {                  
+                JOptionPane.showMessageDialog(null, e);
+            }
+            return resultado;
         }
 }
